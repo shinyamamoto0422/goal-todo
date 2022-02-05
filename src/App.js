1821b61c09a7bb5a09f19a2b2033490c7a7a0b27
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-02-06 00:26:15 +0900

    add:tailwindcssを導入しました

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>これから20日まで頑張っていきましょう！</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline">これから20日まで頑張っていきましょう！</h1>
+    </>
+  );
+}
+
+export default App;
