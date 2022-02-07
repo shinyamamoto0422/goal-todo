803628d8d643fbe1a4fb86bb19311915e85b2371
OAkimasa <80835540+OAkimasa@users.noreply.github.com>
2022-02-07 19:03:26 +0900

    add:3PriorityIcon-#5
    デザインのみの実装
    機能は何もなし

---
+++ src/atomicDesign/atoms/priority/PriorityLow.jsx
@@ -0,0 +1,9 @@
+export const PriorityLow = () => {
+    return (
+        <h1
+            class="text-sm text-center text-gray-800 text-zinc-700  bg-gray-200 rounded-2xl w-8"
+        >
+            低
+        </h1>
+    );
+};
