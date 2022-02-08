cbe695bd21023e8542e5d299bbf98e72aff51152
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-02-08 22:06:50 +0900

    add:TodoAddの変更とtodo入力の詳細要素表示

--- src/components/TodoAdd.jsx
+++ src/components/TodoAdd.jsx
@@ -1,5 +1,12 @@
+import { useState } from 'react';
+import { FlagMarkType1 } from '../atomicDesign/atoms/flagMark/FlagMarkType1';
+import { PriorityThree } from '../atomicDesign/molecules/priority/PriorityThree';
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import DatePicker from 'react-modern-calendar-datepicker';
+
 export const TodoAdd = ({ inputEl, tagEl, handleAddTodoListItem }) => {
-  return (
+  const [ priorityIsShown, setPriorityIsShown ] = useState(false);
+  return ( 
     <>
     {/* textareaはtodoのtitleを入力する要素 */}
       <textarea ref={inputEl} name="" id="" cols="30" rows="5"></textarea>
@@ -9,7 +16,21 @@ export const TodoAdd = ({ inputEl, tagEl, handleAddTodoListItem }) => {
           class="text-white2 text-size-sm text-center w-max-20 h-5"
       >
       </textarea>
-      <button onClick={handleAddTodoListItem}>+todoを追加</button>
+      <FlagMarkType1 />
+      <div class="flex">
+        <button 
+            onMouseOver={() => setPriorityIsShown(true)}
+        >
+          <span>優先度</span>
+          ー
+        </button>
+        {priorityIsShown && (
+          <PriorityThree />
+        )}
+      </div>
+      <textarea defaultValue="メモ" name="" id="" cols="30" rows="2"></textarea>
+      <DatePicker/>
+      <button onClick={handleAddTodoListItem}>追加</button>
     </>
   )
 }
