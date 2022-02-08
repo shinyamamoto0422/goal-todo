45057c880cc95151d99a180a747bd848fb3ba5d4
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-02-09 01:54:15 +0900

    add:ButtonPurple4.jsx

---
+++ src/atomicDesign/atoms/button/ButtonPurple4.jsx
@@ -0,0 +1,7 @@
+export const ButtonPurple4 = ({ content }) => {
+  return (
+    <button className="bg-purple4 hover:bg-purple3 text-white1 font-bold py-2 px-5 rounded-2xl">
+      {content}
+    </button>
+  )
+}
