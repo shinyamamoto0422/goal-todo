1f9f79f1c73ef8a25dea3aafe4de7eb38d43d016
OAkimasa <80835540+OAkimasa@users.noreply.github.com>
2022-02-08 09:44:47 +0900

    Update: FlagMark
    - 塗りつぶしをfigma指定の色へ変更した
    - importでsvgの指定するように変更した

--- src/atomicDesign/atoms/flagMark/FlagMarkType1.jsx
+++ src/atomicDesign/atoms/flagMark/FlagMarkType1.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BookmarkIcon } from '@heroicons/react/outline';
 
 export const FlagMarkType1 = () => {
     const [flagButton, setFlagButton] = useState(false);
@@ -7,20 +8,27 @@ export const FlagMarkType1 = () => {
             <div class="">
                 <button onClick={() => setFlagButton(!flagButton)}>
                     {flagButton ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" color="Indigo-500">
-                            <path
-                                d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z"
-                            />
-                        </svg>
+                        <div class="text-indigo-300">
+                            <BookmarkIcon class="h-8 w-7" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                                />
+                            </BookmarkIcon>
+                        </div>
                     ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
-                            />
-                        </svg>
+                        <div class="text-gray-300">
+                            <BookmarkIcon class="h-8 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                                />
+                            </BookmarkIcon>
+                        </div>
                     )}
                 </button>
             </div>
