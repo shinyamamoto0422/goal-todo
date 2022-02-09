fe91c3defca9a8f5056fe6ab866f50ab241cf93c
OAkimasa <80835540+OAkimasa@users.noreply.github.com>
2022-02-09 14:29:02 +0900

    set-task-style-#41
    ひとつのタスクの中身
    
    - (タスク名) taskName
    -  (リスト名) listName
    -  (重要度) priorityNum
    -  (フラグ) flag
    - (締め切りの時間と日付) deadline
    - (完了・未完了) complete

--- src/atomicDesign/organisms/OneTask.jsx
+++ src/atomicDesign/organisms/OneTask.jsx
@@ -3,19 +3,20 @@ import { Deadline } from "../molecules/Deadline"
 import { TaskLabels } from "../molecules/TaskLabels"
 import { CheckCircleIcon } from "@heroicons/react/outline"
 
-export const OneTask = ({ taskName, listName, priorityNum, flug, timeText, dateText }) => {
-    console.log({ taskName, listName, priorityNum, flug, timeText, dateText })
+export const OneTask = ({ taskName, listName, priorityNum, flag, deadline, complete }) => {
+    //console.log({ taskName, listName, priorityNum, flug, timeText, dateText })
     return (
         <>
             <div class="flex">
-                <div class="text-white1 h-8 w-8 ml-12">
+                <div class="text-white2 h-8 w-8 ml-12">
+                    {/**完了・未完了に合わせて変更する可能性あり (complete) */}
                     <CheckCircleIcon class="h-8 w-8" stroke="currentcolor" />
                 </div>
                 <TaskName>{taskName}</TaskName>
                 <div class="flex h-10">
                     <div class="flex">
-                        <TaskLabels listName={listName} priorityNum={priorityNum} flug={flug} />
-                        <Deadline timeText={timeText} dateText={dateText} />
+                        <TaskLabels listName={listName} priorityNum={priorityNum} flag={flag} />
+                        <Deadline deadline={deadline} />
                     </div>
                 </div>
             </div>
