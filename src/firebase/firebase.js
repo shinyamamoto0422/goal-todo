2b2dc8966dfafdd4c68eeb3f75d630a87d9b38f6
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-02-10 18:42:59 +0900

    add:新しいfirebaseプロジェクトの作成

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_FIREBASE_API_KE",
+  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
   authDomain: "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN",
   projectId: "process.env.REACT_APP_FIREBASE_DATABASE_URL",
   storageBucket: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
