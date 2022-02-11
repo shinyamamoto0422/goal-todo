96ce11fc2a39b520f52b1388106d3316712e9d57
OAkimasa <80835540+OAkimasa@users.noreply.github.com>
2022-02-11 15:40:28 +0900

    途中経過
    一瞬エラー画面が出ます

--- src/atomicDesign/pages/StartPage.jsx
+++ src/atomicDesign/pages/StartPage.jsx
@@ -1,17 +1,80 @@
 import StartLogo from "../../assets/startLogo.svg"
 import { GoogleSignInButton } from "../atoms/button/GoogleSignInButton"
 
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, getRedirectResult } from "firebase/auth";
+
+
 export const StartPage = () => {
+    const signInWithGoogle = () => {
+        const provider = new GoogleAuthProvider()
+
+        const auth = getAuth();
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid;
+                // ...
+            } else {
+                // User is signed out
+                // ...
+            }
+        });
+
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+
+        getRedirectResult(auth)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access Google APIs.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+
+                // The signed-in user info.
+                const user = result.user;
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+
+    }
+
     return (
         <div class="bg-white-1 w-screen h-screen flex justify-center">
-            
-                <div class="h-screen w-auto flex flex-col">
-                    <h1 class="h-1/6" />
-                    <img class="" src={StartLogo} alt="startLogo" />
-                    <h1 class="h-20" />
-                    <GoogleSignInButton/>
-                </div>
-            
+
+            <div
+                onClick={() => signInWithGoogle()}
+                class="h-screen w-auto flex flex-col"
+            >
+                <h1 class="h-1/6" />
+                <img class="" src={StartLogo} alt="startLogo" />
+                <h1 class="h-20" />
+                <GoogleSignInButton />
+            </div>
+
         </div>
     )
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom"
+import { initializeApp } from "firebase/app"
 
 import { HomePage } from "../atomicDesign/pages/HomePage"
 import { MylistPage } from "../atomicDesign/pages/MylistPage"
@@ -6,6 +7,18 @@ import { GoalPage } from "../atomicDesign/pages/GoalPage"
 import { StatsPage } from "../atomicDesign/pages/StatsPage"
 import { AccountPage } from "../atomicDesign/pages/AccountPage"
 import { Page404 } from "../atomicDesign/pages/Page404"
+import { StartPage } from "../atomicDesign/pages/StartPage"
+
+const firebaseConfig = {
+  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
+  authDomain: "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN",
+  projectId: "process.env.REACT_APP_FIREBASE_DATABASE_URL",
+  storageBucket: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
+  messagingSenderId: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
+  appId: "process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
+  measurementId: "process.env.REACT_APP_FIREBASE_APP_ID"
+}
+initializeApp(firebaseConfig);
 
 export default function App() {
   return (
@@ -16,6 +29,9 @@ export default function App() {
         <Route path="/goal" element={<GoalPage />} />
         <Route path="/stats" element={<StatsPage />} />
         <Route path="/account" element={<AccountPage />} />
+
+        <Route path="/signin" element={<StartPage />} />
+
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
