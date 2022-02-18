b8392a9e2961a621b91bdd352c636dfd8d814246
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-02-18 22:41:18 +0900

    add:goal

--- src/atomicDesign/pages/Goal/GoalPage.jsx
+++ src/atomicDesign/pages/Goal/GoalPage.jsx
@@ -3,23 +3,26 @@ import { useState } from 'react';
 import { TopBarBorder } from '../../../common/TopBarBorder';
 import { TopBarText } from '../../organisms/TopBarText';
 import { SideBar } from "../../templates/SideBar"
-import Accordion from './Accordion';
+import { Accordion } from './Accordion';
 // import { ButtonPurple4 } from '../../atoms/button/ButtonPurple4'; 
 
 export const GoalPage = () => {
     const goalList = [
         {
+            id: 0,
             title : "まずはじめに",
             content: "目標設定をし、それを日々こなすことでより夢に近づくお手伝いをします。",
+            placeholder: "",
             advice: "",
             example: "",
-            radius: "rounded-t-md",
-            state: "true",
             isOpen: true,
+            radius: "rounded-t-md",
         },
         {
+            id: 1,
             title : "達成したい目標を掲げる",
-            content: "あなたが自分の人生で達成したいゴールはなんですか？",
+            content: `あなたが自分の人生で達成したいゴールはなんですか？`,
+            placeholder: '私が達成したい目標は…',
             advice: "人はイメージを超える結果は出すことができません。未来のイメージを大きくしましょう！とてつもなく大きな夢でも構いません。",
             example: "",
             isOpen: false,
@@ -36,20 +39,21 @@ export const GoalPage = () => {
             content: "を叶えるために達成しないといけない中間目標はなんですか？（複数）",
             advice: "自分が中目標を達成して行った後に、LifeGoalが見えているのであれば多少飛躍していてもかまいません。",
             example: "",
-            state: "false",
+            isOpen: false,
         },
         {
             title : "数値化してより現実的におとしこむ",
             content: "面倒ですが、もう一度、中間目標を『数値化』に着目して練り直しましょう！",
             advice: "目標を現実のものにするには、明確にすることが大切です。いつまでにやるのか、10個何かを達成するのか、この中間目標は最終目標のどれくらいの割合を占めるのかという風に具体化することで、実際の行動に移しやすくなります。",
             example: "",
-            state: "false",
+            isOpen: false,
         },
         {
             title : "目標に近づくためのルーティンを作る",
             content: "最後に、目標に一歩づつ近づくために習慣化すること（ルーティン行動）を3つ上げましょう！",
             advice: "ルーティンとは「毎日欠かさず行うこと」です。最初は意識しないとできない苦しい行動であっても、続けていると、それは自動化でき日々目標に向かって行動できるようになります。まずは小さなことから始めましょう！",
             example: "",
+            isOpen: false,
             radius: "rounded-b-md",
         },
     ];
@@ -72,11 +76,11 @@ export const GoalPage = () => {
                     <TopBarBorder/>
                 </div>
                 {/* アコーディオン */}
-                <section class="mt-36">
-                    <div class="relative">
+                <section class="mt-24 h-3/4">
+                    <div class="relative h-full">
                         {/* アコーディオンの右側にabsoluteで絶対配置するので、relative消さないで */}
                         {goalList.map((list, index) => (
-                            <Accordion title={list.title} content={list.content} radius={list.radius}/>
+                            <Accordion index={index} title={list.title} content={list.content} placeholder={list.placeholder} radius={list.radius}/>
                         ))}
                     </div>
                 </section>
@@ -84,7 +88,3 @@ export const GoalPage = () => {
         </div>
     )
 };
-
-// できるだけコード量は減らしたい
-// 勢いで、最初からこの書き方でやってしまった。
-// 
